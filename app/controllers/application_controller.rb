da7701e19bc78de6bd3c26865a7dde5cc2ceea6d
Mary Lin <marylin@chicagobooth.edu>
2017-06-01 08:33:30 -0500

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,19 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, :keys => [:username, :avatar_url])
+  protect_from_forgery with: :exception
 
-    devise_parameter_sanitizer.permit(:account_update, :keys => [:avatar_url])
+  before_action :authenticate_user!
 
-  end
+  # before_action :configure_permitted_parameters, if: :devise_controller?
+  #
+  # protected
+  #
+  # def configure_permitted_parameters
+  #   devise_parameter_sanitizer.permit(:sign_up, :keys => [:username, :avatar_url])
+  #
+  #   devise_parameter_sanitizer.permit(:account_update, :keys => [:avatar_url])
+  #
+  # end
 
 end
